123330b786ac3b5d07ace3321c2afa93e39cb19c
Diamekod0221 <diamekod0221@gmail.com>
2023-11-01 23:11:32 +0100

    Added sql scripts

--- sql_scripts/create_schemas.sql
+++ sql_scripts/create_schemas.sql
@@ -14,7 +14,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema cinema
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `cinema` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+CREATE SCHEMA IF NOT EXISTS `cinema` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
 USE `cinema` ;
 
 -- -----------------------------------------------------
